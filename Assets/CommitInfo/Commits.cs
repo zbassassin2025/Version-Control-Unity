1570fbfde0cf1c6c9a563d8928ebc11561c5ec47
zbogner-snhu <z.bogner@snhu.edu>
2025-02-26 10:20:37 -0700

    Add commit for Assets creation and c# script

---
+++ Assets/CommitInfo/Commits.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commits : MonoBehaviour
+{
+   // Commits 
+   /*
+    Create commits and push changes to repo. 
+
+    */ 
+}
